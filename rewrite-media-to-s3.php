2182898442fdae0ca9877989a37003a7ba30ee74
Damien Wilson <damien.wilson@digital.justice.gov.uk>
2020-03-27 16:00:19 +0000

    Remove var_dump

--- rewrite-media-to-s3.php
+++ rewrite-media-to-s3.php
@@ -18,9 +18,7 @@ define('S3_SIGN_URLS', env('S3_SIGN_URLS') ?: false);
 // Instantiate the class and register hooks
 if (defined('S3_UPLOADS_BASE_URL') && !empty(S3_UPLOADS_BASE_URL)) {
     $signed = null;
-    var_dump($signed);
     if (S3_SIGN_URLS !== false) {
-        var_dump(S3_SIGN_URLS);
         $signed = new Signature();
     }
     $UrlRewriter = new UrlRewriter($localBase, S3_UPLOADS_BASE_URL, $signed);
